7e3f9ba69b786087091bf88b8401402d8b9ed03c
jmoseka <jamylam3@gmail.com>
2022-07-18 22:54:24 +0300

    Rubocop fix error

--- student.rb
+++ student.rb
@@ -1,10 +1,10 @@
 require './person'
 
 class Student < Person
-    def initialize(age, classroom,  name = 'Unknown', parent_permission: true)
-      super(age, name, parent_permission)
-      @classroom = classroom
-    end
+  def initialize(age, classroom, name = 'Unknown', parent_permission: true)
+    super(age, name, parent_permission)
+    @classroom = classroom
+  end
 
   def play_hooky
     "¯\(ツ)/¯"
