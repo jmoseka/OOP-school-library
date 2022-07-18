52d38b8b4554e484629741a1e5bd47b9371143b1
jmoseka <jamylam3@gmail.com>
2022-07-18 19:42:49 +0300

    Fix rubicop errors

--- person.rb
+++ person.rb
@@ -1,21 +1,13 @@
 class Person
-    def initialize
-        @id = "#{("a".."z").to_a.sample} #{rand(1...100)}"
-        @name = "User #{rand(1..100)}"
-        @age = "#{rand(18..100)}"
-      end
+  def initialize
+    @id = "#{('a'..'z').to_a.sample} #{rand(1...100)}"
+    @name = "User #{rand(1..100)}"
+    @age = rand(18..100).to_s
+  end
 
-      # getter method
-      def id
-        @id
-      end
+  # getter method
+  attr_reader :id
 
-      def name
-        @name
-      end
-
-      def age
-        @age
-      end
-      # ----------end---------
+  attr_reader :name, :age
+  # ----------end---------
 end
