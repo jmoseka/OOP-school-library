3fbe31b00b23382465455fce8c98c5004c065dff
jmoseka <jamylam3@gmail.com>
2022-07-18 19:30:42 +0300

    Add getter methods for age, id and name

--- person.rb
+++ person.rb
@@ -3,4 +3,19 @@ class Person
         @id = "#{("a".."z").to_a.sample} #{rand(1...100)}"
         @name = "User #{rand(1..100)}"
         @age = "#{rand(18..100)}"
+      end
+
+      # getter method
+      def id
+        @id
+      end
+
+      def name
+        @name
+      end
+
+      def age
+        @age
+      end
+      # ----------end---------
 end
