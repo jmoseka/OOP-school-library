12c67f429fb610dfc40a4b8b0da1d7977b92e2d1
jmoseka <jamylam3@gmail.com>
2022-07-20 14:38:03 +0300

    Person class inherits from Nameable

--- person.rb
+++ person.rb
@@ -1,8 +1,11 @@
-class Person
+require './nameable'
+
+class Person < Nameable
   attr_accessor :name, :age
   attr_reader :id
 
   def initialize(age, name = 'Unknown', parent_permission: true)
+    super()
     @id = Random.rand(1..1000)
     @name = name
     @age = age
@@ -17,5 +20,9 @@ class Person
 
   def of_age?
     age >= 18
+  end
+
+  def correct_name
+    @name
   end
 end
