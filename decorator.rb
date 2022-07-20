92fce441ec2d88b8bc6a3a422777815fe7bb7a80
jmoseka <jamylam3@gmail.com>
2022-07-20 14:43:42 +0300

    BPrepare base DecoratorB

---
+++ decorator.rb
@@ -0,0 +1,18 @@
+require './nameable'
+# The base Decorator class follows the same interface as the other components.
+# The primary purpose of this class is to define the wrapping interface for all
+# concrete decorators. The default implementation of the wrapping code might
+# include a field for storing a wrapped component and the means to initialize
+# it.
+class Decorator < Nameable
+    attr_accessor :component
+    super()
+    initialize(nameable)
+      @nameable = nameable
+    end
+  
+    # The Decorator delegates all work to the wrapped component.
+    def correct_name
+      @nameable.correct_name.upcase
+    end
+  end
