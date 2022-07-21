d13ea366b2b7cd5b796a5a822bd069e002a46843
jmoseka <jamylam3@gmail.com>
2022-07-21 14:07:57 +0300

    Create a class Book

--- classroom.rb
+++ classroom.rb
@@ -1,15 +1,15 @@
 class Classroom
-  attr_accessor :label, :
+  attr_accessor :label, :student
 
   def initialize(label)
     @label = label
     @student = []
   end
 
-  def has_many(student)
-    # saving each student object in an array 
+  def many(student)
+    # saving each student object in an array
     @students << student
-    # saving the current classroom object refered to as self 
+    # saving the current classroom object refered to as self
     # to the student objects classroom variable
     student.classroom = self
   end
