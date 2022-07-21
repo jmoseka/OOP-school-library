0770cd512ed22de97859ee675aa6775d9091a1aa
jmoseka <jamylam3@gmail.com>
2022-07-21 13:59:49 +0300

    Add relationship between Classroom and Student

--- classroom.rb
+++ classroom.rb
@@ -1,7 +1,16 @@
 class Classroom
-  attr_accessor :label
+  attr_accessor :label, :
 
   def initialize(label)
     @label = label
+    @student = []
+  end
+
+  def has_many(student)
+    # saving each student object in an array 
+    @students << student
+    # saving the current classroom object refered to as self 
+    # to the student objects classroom variable
+    student.classroom = self
   end
 end
