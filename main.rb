27f5b3d505e003560e5ea1aea751db0248093e60
jmoseka <jamylam3@gmail.com>
2022-07-23 19:34:42 +0300

    Add main function

--- main.rb
+++ main.rb
@@ -1,28 +1,10 @@
-def select_options
-  puts 'Please choose an option by entering a number:'
-  puts '1 - List all books'
-  puts '2 - List all people'
-  puts '3 - Create a person'
-  puts '4 - Create a book'
-  puts '5 - Create a rental'
-  puts '6 - List all rentals for a given person id'
-  puts '7 - Exit'
-  puts ''
-end
+require './app'
 
 def main
   puts 'Welcome to School Librabry App!'
   puts ''
-
-  input = nil
-
-  while input != 7
-    select_options
-    input = gets.chomp.to_i
-
-    # code to be executed
-  end
-
+  app = App.new
+  app.run
 end
 
 main
